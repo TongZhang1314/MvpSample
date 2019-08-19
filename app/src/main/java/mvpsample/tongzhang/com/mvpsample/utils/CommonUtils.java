2bbd921fea24a416ed27f48937044d6ef97e17a0
TongZhang1314 <zz791672885@163.com>
2019-08-19 17:35:00 +0800

    扩展工具类

--- app/src/main/java/mvpsample/tongzhang/com/mvpsample/utils/CommonUtils.java
+++ app/src/main/java/mvpsample/tongzhang/com/mvpsample/utils/CommonUtils.java
@@ -33,6 +33,16 @@ public class CommonUtils {
     }
 
     /**
+     * convert px to its equivalent dp
+     *
+     * 将px转换为与之相等的dp
+     */
+    public static int px2dp(Context context, float pxValue) {
+        final float scale =  context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
      * Show message
      *
      * @param activity Activity

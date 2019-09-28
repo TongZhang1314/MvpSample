5ae2b3a15abfe1d0318f3facdfec29e2f85e0d59
TongZhang1314 <zz791672885@163.com>
2019-09-28 17:59:16 +0800

    test git

--- app/src/main/java/mvpsample/tongzhang/com/mvpsample/ui/home/fragment/HomeFragment.java
+++ app/src/main/java/mvpsample/tongzhang/com/mvpsample/ui/home/fragment/HomeFragment.java
@@ -1,12 +1,14 @@
 package mvpsample.tongzhang.com.mvpsample.ui.home.fragment;
 
+import android.os.AsyncTask;
+
 import mvpsample.tongzhang.com.mvpsample.base.BaseFragment;
 import mvpsample.tongzhang.com.mvpsample.base.contract.home.HomeContract;
 
 public class HomeFragment extends BaseFragment<HomeContract.Presenter> implements HomeContract.View {
     @Override
     protected void initPresenter() {
-
+    //test git
     }
 
     @Override

e85d23642267c19d5ede1059920d5d367d94fbe1
TongZhang1314 <zz791672885@163.com>
2019-09-30 17:51:17 +0800

    编写homefragment

---
+++ app/src/main/java/mvpsample/tongzhang/com/mvpsample/presenter/home/HomePresenter.java
@@ -0,0 +1,15 @@
+package mvpsample.tongzhang.com.mvpsample.presenter.home;
+import mvpsample.tongzhang.com.mvpsample.base.RxPresenter;
+import mvpsample.tongzhang.com.mvpsample.base.contract.home.HomeContract;
+
+public class HomePresenter extends RxPresenter<HomeContract.View> implements HomeContract.Presenter {
+    @Override
+    public void attachView(HomeContract.View view) {
+
+    }
+
+    @Override
+    public void detachView() {
+
+    }
+}
--- app/src/main/java/mvpsample/tongzhang/com/mvpsample/ui/home/fragment/HomeFragment.java
+++ app/src/main/java/mvpsample/tongzhang/com/mvpsample/ui/home/fragment/HomeFragment.java
@@ -1,20 +1,28 @@
 package mvpsample.tongzhang.com.mvpsample.ui.home.fragment;
 
 import android.os.AsyncTask;
+import android.support.v7.widget.RecyclerView;
 
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+
+import butterknife.BindView;
+import mvpsample.tongzhang.com.mvpsample.R;
 import mvpsample.tongzhang.com.mvpsample.base.BaseFragment;
 import mvpsample.tongzhang.com.mvpsample.base.contract.home.HomeContract;
 
 public class HomeFragment extends BaseFragment<HomeContract.Presenter> implements HomeContract.View {
+    @BindView(R.id.srl_main)
+    SmartRefreshLayout srlMain;
+    @BindView(R.id.rv_main)
+    RecyclerView rvMain;
     @Override
     protected void initPresenter() {
-    //test git
-        //
+
     }
 
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.fragment_home;
     }
 
     @Override
